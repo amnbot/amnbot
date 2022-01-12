fc7ababb49784c1a36588aa7a02eec8664ee7e01
AMNbot <aymenouali01@gmail.com>
2022-01-12 17:47:12 -0500

    fixed project icons

--- src/components/ui/ProjectStack.svelte
+++ src/components/ui/ProjectStack.svelte
@@ -4,6 +4,6 @@
 
 <div class="flex sm:flex-row flex-col items-center justify-center">
     {#each stack as fw}
-    <img class="w-32 m-5 transition-transform hover:scale-110" src={fw} alt="image"/>
+    <img class="w-[10%] m-5 transition-transform hover:scale-110" src={fw} alt="image"/>
     {/each}
 </div>
