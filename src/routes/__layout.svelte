ca923fae5c6a59eaa255c1debc065d55bf7577ac
AMNbot <aymenouali01@gmail.com>
2022-01-12 19:06:49 -0500

    fixed bottom page and personal website image

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,13 +3,16 @@
     import Socials from "../components/functional/socials.svelte"
     import About from "./about.svelte"
 </script>
-<div class="min-h-[screen] bg-[#1a1620] ">
+<div class="min-h-screen bg-[#1a1620] ">
     <div class="px-4 max-w-4xl mx-auto content-center text-center"> 
         
         <Nav />
         <h1 class="font-thin tracking-widest uppercase text-7xl text-white">Aymen Ouali</h1>
         <Socials />
         <slot></slot>
+    </div>
+    <div class="pb-10">
+
     </div>
     
  </div>
